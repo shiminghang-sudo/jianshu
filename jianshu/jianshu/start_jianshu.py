82e44f32acd934d1cc679c2af909f9a8425e2baf
shiminghang_sudo <1518532557@qq.com>
2020-01-04 19:04:02 +0800

    更新start_jianshu.py文件

--- jianshu/jianshu/start_jianshu.py
+++ jianshu/jianshu/start_jianshu.py
@@ -2,7 +2,7 @@ from scrapy.cmdline import execute
 
 execute("scrapy crawl jianshu_spider".split(" "))
 
-# import pymysql
-#
-# db = pymysql.connect(host="127.0.0.1", user='root', password="smh8532557", database='zoo')
-# print(db)
+import pymysql
+
+db = pymysql.connect(host="127.0.0.1", user='root', password="smh8532557", database='zoo')
+print(db)
